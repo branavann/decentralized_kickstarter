db9a50e6f8daaf580b80e62066d3e6d1653b272b
Branavan Nagarajan <branavannagarajan@gmail.com>
2021-05-06 18:12:45 -0400

    Request struct creation and functions associated with Request

--- contracts/Campaign.sol
+++ contracts/Campaign.sol
@@ -2,9 +2,22 @@ pragma solidity^0.4.17;
 
 contract Campaign {
 
+    struct Request {
+        string description;
+        uint value;
+        address recipient;
+        bool complete;
+    }
+
     address public manager;
     uint public minimumContribution;
     address[] public approvers;
+    Request[] public requests;
+
+    modifier onlyManager() {
+        require(msg.sender == manager);
+        _;
+    }
 
     function Campaign(uint minimum) public {
         manager = msg.sender;
@@ -14,5 +27,15 @@ contract Campaign {
     function contribute() public payable {
         require(msg.value > minimumContribution);
         approvers.push(msg.sender);
+    }
+
+    function createRequest(string description, uint value, address recipient) public onlyManager {
+        Request newRequest = Request({
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: bool
+        });
+        requests.push(newRequest);
     }
 }
