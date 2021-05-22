9ec3924927151b2c092588e05ff7e8d9e919bf13
Branavan Nagarajan <branavannagarajan@gmail.com>
2021-05-22 12:15:58 -0400

    Finalized request homepage

--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -2,24 +2,72 @@ import React, { Component } from 'react';
 import Layout from '../../../components/Layout';
 import Campaign from '../../../ethereum/campaign';
 import { Link } from '../../../routes';
-import { Button } from 'semantic-ui-react';
+import { Button, Table } from 'semantic-ui-react';
+import RequestRow from '../../../components/RequestRow';
 
 class RequestIndex extends Component {
 
     static async getInitialProps(props) {
         const { address } = props.query;
-        return { address };
+        const campaign = Campaign(address);
+        const requestCount = await campaign.methods.getRequestsCount().call();
+        // Used for RequestRow component 
+        const contributorsCount = await campaign.methods.contributorsCount().call();
+        // Gathering the requests for the specified campaign
+        const requests = await Promise.all(
+            Array(parseInt(requestCount)).fill().map((element, index) => {
+                return campaign.methods.requests(index).call();
+            })
+        );
+
+        return { address, requests, requestCount, contributorsCount };
+    }
+
+    renderRows() {
+        return this.props.requests.map((request,index) => {
+            return <RequestRow 
+                key={index}
+                id={index}
+                request={request}
+                address={this.props.address}
+                contributorsCount={this.props.contributorsCount}
+                />;
+        });
     }
 
     render() {
+
+        const { Header, Row, HeaderCell, Body } = Table;
+
+
         return (
             <Layout>
               <h3>Requests</h3>
+              <Link route={`/campaigns/${this.props.address}`}>
+                <a>Back</a>
+              </Link>
               <Link route={`/campaigns/${this.props.address}/requests/new`}>
                 <a>
-                    <Button primary>New Request</Button>
+                    <Button secondary floated='right' style={{ marginBottom: 10 }}>New Request</Button>
                 </a>
               </Link>
+              <Table>
+                  <Header>
+                      <Row>
+                          <HeaderCell>ID</HeaderCell>
+                          <HeaderCell>Description</HeaderCell>
+                          <HeaderCell>Amount</HeaderCell>
+                          <HeaderCell>Recipient</HeaderCell>
+                          <HeaderCell>Approval Count</HeaderCell>
+                          <HeaderCell>Approve</HeaderCell>
+                          <HeaderCell>Finalize</HeaderCell>
+                      </Row>
+                  </Header>
+                  <Body>
+                      {this.renderRows()}
+                  </Body>
+              </Table>
+              <div>Found {this.props.requestCount} requests.</div>
             </Layout>
         );
     }
--- pages/campaigns/requests/new.js
+++ pages/campaigns/requests/new.js
@@ -6,24 +6,77 @@ import { Button, Form, Input, Message } from 'semantic-ui-react';
 import { Link, Router } from '../../../routes';
 
 class RequestNew extends Component {
+
+    state = {
+        value: '',
+        description: '',
+        receipient: '',
+        errorMessage: '',
+        loading: false
+    }
+
     static async getInitialProps(props) {
         const { address } = props.query;
         return { address };
     }
 
+    onSubmit = async event => {
+        event.preventDefault();
+
+        // Gathering the instance of our campaign
+        const campaign = Campaign(this.props.address);
+        // Destructuring our state variables 
+        const { value, description, receipient } = this.state;
+
+        this.setState({ errorMessage: '', loading: true });
+        try {
+            const accounts = await web3.eth.getAccounts();
+            await campaign.methods.createRequest(description, web3.utils.toWei(value, 'ether'), receipient).send({ from: accounts[0] });
+            // Navigate users back to the requests home page
+            Router.pushRoute(`/campaigns/${this.props.address}/requests`);
+        } catch (err) {
+            this.setState({ errorMessage: err.message });
+        };
+
+        // Stops the spinner on the button 
+        this.setState({ loading: false });
+    };
+
     render() {
         return (
             <Layout>
-               <Form>
+                <Link route={`/campaigns/${this.props.address}/requests`}>
+                    <a>
+                        Back
+                    </a>
+                </Link>
+                <h3>Create a New Request</h3>
+               <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
                    <Form.Field>
                        <label>Description</label>
+                       <Input 
+                           value={this.state.description}
+                           onChange={event => this.setState({ description: event.target.value })} 
+                       />
                    </Form.Field>
                    <Form.Field>
                        <label>Value (Ether)</label>
+                       <Input 
+                            value={this.state.value}
+                            onChange={event => this.setState({ value: event.target.value })}
+                        />
                    </Form.Field>
                    <Form.Field>
                        <label>Recipient Address</label>
+                       <Input 
+                            value={this.state.receipient}
+                            onChange={event => this.setState({ receipient: event.target.value })}
+                       />
                    </Form.Field>
+                   <Message error header="Could not process your request" content={this.state.errorMessage} />
+                   <Button loading={this.state.loading} secondary>
+                        Create
+                   </Button>
                </Form>
             </Layout>
             
